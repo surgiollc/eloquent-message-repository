a4e751c7fb96f4bd6e748c1aacc306e1c76ba5c4
Jos <sellis.jos@gmail.com>
2018-06-21 16:51:44 +0000

    Apply fixes from StyleCI

--- src/EventSauceServiceProvicer.php
+++ src/EventSauceServiceProvicer.php
@@ -4,15 +4,16 @@ namespace JSellis\EloquentMessageRepository;
 
 use Illuminate\Support\ServiceProvider;
 
-class EventSauceServiceProvider extends ServiceProvider
+class EventSauceServiceProvicer extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../migrations');
     }
+
     /**
      * Register the application services.
      */
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace JSellis\EloquentMessageRepository\Tests;
 
-use EventSauce\EventSourcing\Serialization\SerializableEvent;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
@@ -12,7 +11,7 @@ class TestCase extends OrchestraTestCase
         $app['config']->set('database.default', 'testing');
         $app['config']->set('database.connections.testing', [
             'driver' => 'sqlite',
-            'database' => __DIR__ . '/database.sqlite',
+            'database' => __DIR__.'/database.sqlite',
         ]);
     }
 }
